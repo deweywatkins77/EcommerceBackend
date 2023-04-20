3941f06d6e4a182e4de757245772194c6ea4e005
Dewey Watkins <deweywatkins77@gmail.com>
2023-04-20 00:42:00 -0400

    product routes are working

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,14 +4,40 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all products
-  // be sure to include its associated Category and Tag data
+  try {
+    let results = await Product.findAll({
+      include:[
+        {model: Category, attributes: ['category_name'] },
+        {model: Tag, attributes: ['tag_name'], through:{attributes:[]}}
+      ]
+    })
+    res.status(200).json(results)
+  }catch{((err) => {
+    console.log(err);
+    res.status(400).json(err);
+    });
+  }
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
+  try {
+    let results = await Product.findOne({
+      include:[
+        {model: Category, attributes: ['category_name'] },
+        {model: Tag, attributes: ['tag_name'], through:{attributes:[]}}
+      ],
+      where:[{id:req.params.id}]
+    })
+    res.status(200).json(results)
+  }catch{((err) => {
+    console.log(err);
+    res.status(400).json(err);
+    });
+  }
   // be sure to include its associated Category and Tag data
 });
 
@@ -89,8 +115,17 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
-  // delete one product by its `id` value
+router.delete('/:id', async(req, res) => {
+  try {
+    let results = await Product.destroy({
+      where:[{id:req.params.id}]
+    })
+    res.status(200).json(results)
+  }catch{((err) => {
+    console.log(err);
+    res.status(400).json(err);
+    });
+  }
 });
 
 module.exports = router;
